53fba9af35a3c99ce3bc36c7a346c1ee986ab877
MaryamMohamedYahya <147263523+MaryamMohamedYahya@users.noreply.github.com>
2025-01-04 18:11:49 +0530

    Update Api.js

--- Api.js
+++ Api.js
@@ -1,2 +1,21 @@
+
+
 const API_KEY=""
 const API_URL="https://api.nasa.gov/planetary/apod?api_key=${API_KEY}";
+const contentDiv = document.getElementByID("content");
+async function fetchAPOD(){
+  try{
+    const response = await fetch(API_URL);
+    const data = await response.json();
+
+    contentDiv.innerHTML = '
+      <h2>${data.title}</h2>
+      <img>src="${data.url}" alt="${data.title}">
+      <p>${data.explanation}</p>
+    ;
+  }
+   catch (error) {
+     contentDiv.innerHTML = '<p>Error fetching data: ${error.message}</p>';
+   }
+    
+  }
