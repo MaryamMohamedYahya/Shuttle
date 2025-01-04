027e78476985be62d801488efb1d16aae857f1d3
MaryamMohamedYahya <MaryamMohamedYahya@users.noreply.github.com>
2025-01-04 19:54:39 +0530

    Output

--- Api.js
+++ Api.js
@@ -1,7 +1,7 @@
 
 
-const API_KEY="olm0JCCg4670vgWebfFQ7okYu5ZicQfZ74Lrj5Un"
-const API_URL="https://api.nasa.gov/planetary/apod?api_key=${API_KEY}";
+const API_KEY="olm0JCCg4670vgWebfFQ7okYu5ZicQfZ74Lrj5Un";
+const API_URL=`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`;
 const contentDiv = document.getElementById("content");
 async function fetchAPOD(){
   try{
@@ -10,12 +10,12 @@ async function fetchAPOD(){
 
     contentDiv.innerHTML = 
       `<h2>${data.title}</h2>
-      <img>src="${data.url}" alt="${data.title}">
-      <p>${data.explanation}</p>`
-    ;
+      <img src="${data.url}" alt="${data.title}">
+      <p>${data.explanation}</p>`;
   }
    catch (error) {
      contentDiv.innerHTML = `<p>Error fetching data: ${error.message}</p>`;
    }
     
   }
+  fetchAPOD();
